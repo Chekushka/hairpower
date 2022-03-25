1fe9f4fef50fb8ab01718b4d0a47b98f3753579d
Chekushka <chekun.sergiy@gmail.com>
2022-03-25 19:25:51 +0200

    Project init.

---
+++ Assets/Scripts/CameraChanging.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraChanging : MonoBehaviour
+{
+    [SerializeField] private List<CinemachineVirtualCamera> cameras;
+   
+    public void ChangeCamera(CameraType cameraType)
+    {
+        SetCameraPrioritiesToZero();
+        cameras[(int)cameraType].Priority = 1;
+    }
+
+    private void SetCameraPrioritiesToZero()
+    {
+        foreach (var cam in cameras)
+            cam.Priority = 0;
+    }
+}
+
+public enum CameraType
+{
+    Main,
+}
