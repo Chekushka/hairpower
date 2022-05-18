aa23bfd7a5e0376551e233fa3be6cfe9223d655f
Chekushka <chekun.sergiy@gmail.com>
2022-05-18 23:52:38 +0300

    Added fog for far objects. Changed camera pos. Changed on tap pointer sprite. Changed jump obstacle prefab

--- Assets/Scripts/CameraLookPointPositioning.cs
+++ Assets/Scripts/CameraLookPointPositioning.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 public class CameraLookPointPositioning : MonoBehaviour
 {
     [SerializeField] private Transform hips;
+    [SerializeField] private Vector3 offset;
 
     private CharacterMovement _characterMovement;
     private Transform _followTransform;
-
+    
     private void Start()
     {
         _characterMovement = FindObjectOfType<CharacterMovement>();
         _followTransform = _characterMovement.transform;
     }
 
-    public void EnableCameraFollowHips() => _followTransform = hips;
-    public void DisableCameraFollowHips() => _followTransform = _characterMovement.transform;
-
     private void Update() => transform.position = 
-        new Vector3(transform.position.x, transform.position.y, _followTransform.position.z);
+        new Vector3(_followTransform.position.x, transform.position.y, _followTransform.position.z) + offset;
 }
